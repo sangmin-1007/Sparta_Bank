fd1010e9f644490d7abe6bfb1fba2ea003bdae3f
sangmin-1007 <dosin0822@gmail.com>
2024-02-06 11:52:10 +0900

    Feat : 금액을 입력받아 입,출금 하는 기능 구현
    금액을 입력받아 입금 혹은 출금하는 기능 구현 (숫자만 입력받게 제한)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,9 +13,12 @@ public class GameManager : MonoBehaviour
     [Header("Text")]
     [SerializeField] private Text myMoneyTxt;
     [SerializeField] private Text bankMoneyTxt;
+    [SerializeField]private InputField[] inputText;
 
     private int myMoney;
     private int bankMoney;
+
+    private bool isDeposit;
 
     private void Start()
     {
@@ -27,12 +30,14 @@ public class GameManager : MonoBehaviour
 
     public void OnClickDepositBtn()
     {
+        isDeposit = true;
         buttonObject.SetActive(false);
         depositObject.SetActive(true);
     }
 
     public void OnClickWithDrawBtn()
     {
+        isDeposit = false;
         buttonObject.SetActive(false);
         withDrawObject.SetActive(true);
         
@@ -44,4 +49,113 @@ public class GameManager : MonoBehaviour
         depositObject.SetActive(false);
         buttonObject.SetActive(true);
     }
+
+    #region 금액별 버튼클릭 메서드
+    public void OnClick10000Btn()
+    {
+        if(isDeposit)
+        {
+            if(myMoney >= 10000)
+            {
+                myMoney -= 10000;
+                bankMoney += 10000;
+                myMoneyTxt.text = myMoney.ToString("N0");
+                bankMoneyTxt.text = $"Balance    {bankMoney.ToString("N0")}";
+            }
+        }
+        else
+        {
+            if(bankMoney >= 10000)
+            {
+                bankMoney -= 10000;
+                myMoney += 10000;
+                myMoneyTxt.text = myMoney.ToString("N0");
+                bankMoneyTxt.text = $"Balance    {bankMoney.ToString("N0")}";
+            }
+        }
+    }
+
+    public void OnClick30000Btn()
+    {
+        if (isDeposit)
+        {
+            if (myMoney >= 30000)
+            {
+                myMoney -= 10000;
+                bankMoney += 10000;
+                myMoneyTxt.text = myMoney.ToString("N0");
+                bankMoneyTxt.text = $"Balance    {bankMoney.ToString("N0")}";
+            }
+        }
+        else
+        {
+            if (bankMoney >= 30000)
+            {
+                bankMoney -= 30000;
+                myMoney += 30000;
+                myMoneyTxt.text = myMoney.ToString("N0");
+                bankMoneyTxt.text = $"Balance    {bankMoney.ToString("N0")}";
+            }
+        }
+    }
+
+    public void OnClick50000Btn()
+    {
+        if (isDeposit)
+        {
+            if (myMoney >= 50000)
+            {
+                myMoney -= 50000;
+                bankMoney += 50000;
+                myMoneyTxt.text = myMoney.ToString("N0");
+                bankMoneyTxt.text = $"Balance    {bankMoney.ToString("N0")}";
+            }
+        }
+        else
+        {
+            if (bankMoney >= 50000)
+            {
+                bankMoney -= 50000;
+                myMoney += 50000;
+                myMoneyTxt.text = myMoney.ToString("N0");
+                bankMoneyTxt.text = $"Balance    {bankMoney.ToString("N0")}";
+            }
+        }
+    }
+
+    public void OnClickInputDeposit()
+    {
+        bool isPossible = int.TryParse(inputText[0].text, out int getMoney);
+        if (!isPossible)
+        {
+            Debug.Log("숫자만 입력하세요");
+            return;
+        }
+        else if (myMoney >= getMoney)
+        {
+            myMoney -= getMoney;
+            bankMoney += getMoney;
+            myMoneyTxt.text = myMoney.ToString("N0");
+            bankMoneyTxt.text = $"Balance    {bankMoney.ToString("N0")}";
+        }
+    }
+    public void OnClickInputWithDraw()
+    {
+        bool isPossible = int.TryParse(inputText[1].text, out int getMoney);
+        if (!isPossible)
+        {
+            Debug.Log("숫자만 입력하세요");
+            return;
+        }
+        else if (bankMoney >= getMoney)
+        {
+            myMoney += getMoney;
+            bankMoney -= getMoney;
+            myMoneyTxt.text = myMoney.ToString("N0");
+            bankMoneyTxt.text = $"Balance    {bankMoney.ToString("N0")}";
+        }
+    }
+    #endregion
+
+
 }
