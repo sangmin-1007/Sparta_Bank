a1333699f6f3a44f26bdb0730ecf17226d76a6cb
sangmin-1007 <dosin0822@gmail.com>
2024-02-06 11:21:52 +0900

    Feat : 초기 현금,저금된 금액 설정 + 통화 단위 적용
    초기 현금 10만원, 은행에 저금된돈 5만원
    
    통화 단위 적용 1000의 자리마다 , 표시

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [Header("бс GameObject")]
-    [SerializeField]private GameObject buttonObject;
+    [SerializeField] private GameObject buttonObject;
     [SerializeField] private GameObject depositObject;
     [SerializeField] private GameObject withDrawObject;
 
+    [Header("Text")]
+    [SerializeField] private Text myMoneyTxt;
+    [SerializeField] private Text bankMoneyTxt;
 
+    private int myMoney;
+    private int bankMoney;
 
+    private void Start()
+    {
+        myMoney = 100000;
+        bankMoney = 50000;
+        myMoneyTxt.text = myMoney.ToString("N0");
+        bankMoneyTxt.text = $"Balance    {bankMoney.ToString("N0")}";
+    }
 
     public void OnClickDepositBtn()
     {
